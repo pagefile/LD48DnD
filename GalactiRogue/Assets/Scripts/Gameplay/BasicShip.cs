7615cf490e4a486a19afac0f3e075995f68454d2
pagefile <ahgaudette@gmail.com>
2021-04-24 15:40:09 -0500

    Made turning better
    Turn rate can more easily be tweaked in inspector. There's a bit of black magic going on though. Understanding of the underlying physics engine would help.

--- GalactiRogue/Assets/Scripts/Gameplay/BasicShip.cs
+++ GalactiRogue/Assets/Scripts/Gameplay/BasicShip.cs
@@ -7,6 +7,8 @@ public class BasicShip : MonoBehaviour, IBasicShipControl
 {
     [SerializeField]
     private float _acceleration = 1f;
+    [SerializeField]
+    private float _turnAccel = 2f;
 
     private Rigidbody _rb;
     private float _throttle = 0f;
@@ -80,8 +82,17 @@ public class BasicShip : MonoBehaviour, IBasicShipControl
         float accel = _acceleration * _throttle;
         _rb.AddForce(transform.forward * accel, ForceMode.Acceleration);
 
-        float turn = _acceleration * _latAxis;
-        _rb.AddTorque(Vector3.up * _latAxis, ForceMode.Acceleration);
+        float turn = _turnAccel * _latAxis;
+
+        // I don't understand what is happening here, but this is the only form of the function call that
+        // gives me what I want. Using ForceMode.Impulse or ForceMode.Force causes the parent connstraint on
+        // the camera to wig out and slingshot around while the ship is spinning. This doesn't
+        // happen with ForceMode.Acceleration for some reason, and at first glace it seems the
+        // physics of it are all basically the same, except the camera doesn't wig out.
+        // This is also a formula I'd use with ForceMode.Impulse and not ForceMode.Accerlation, so this
+        // Really is a case of "My code works and I don't understand why". If it works don't
+        // fix it? 
+        _rb.AddTorque(Vector3.up * _latAxis * _rb.mass * Time.deltaTime, ForceMode.Acceleration);
     }
     #endregion
 }
