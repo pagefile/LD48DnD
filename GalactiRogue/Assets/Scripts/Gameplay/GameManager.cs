9a575d840b049ec673a6a07a3cba2e9ca8cfc0d7
pagefile <ahgaudette@gmail.com>
2021-04-24 23:13:10 -0500

    Sector Grid Generation
    Generates a grid of indices to be used for generating a sector based on parameters

--- GalactiRogue/Assets/Scripts/Gameplay/GameManager.cs
+++ GalactiRogue/Assets/Scripts/Gameplay/GameManager.cs
@@ -7,7 +7,8 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField]
     private BasicShip _player = default;
-    private Scene _activeScene;
+
+    private int[,] sectorGrid = null;
 
     // Start is called before the first frame update
     void Start()
@@ -30,11 +31,27 @@ public class GameManager : MonoBehaviour
         operation.completed -= NewSectorLoaded;
         // Do things
         Debug.Log("Scene loaded");
+
+        // Generate the sector
+        sectorGrid = SectorGenerator.GenerateSectorGrid(1000f, 100f, 50f, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(sectorGrid != null)
+        {
+            for(int i = 0; i < sectorGrid.GetLength(0); i++)
+            {
+                for(int j = 0; j < sectorGrid.GetLength(1); j++)
+                {
+                    if(sectorGrid[i, j] >= 0)
+                    {
+                        Vector3 pos = new Vector3(i * 100f, 0f, j * 100f) - new Vector3(550f, 0f, 550f);
+                        Debug.DrawLine(_player.transform.position, pos, Color.red);
+                    }
+                }
+            }
+        }
     }
 }
---
+++ GalactiRogue/Assets/Scripts/Systems/SectorGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SectorGenerator
+{
+    // size is the dimensions of the sector in meters (making it a square
+    // cellSize is the size in meters of each individual grid unit
+    // density is the chance for a POI. Nothing is represented by a negative number, otherwise a valid array index is used
+    // maxIndex is the highest index for the array containing POIs to generate from
+    public static int[,] GenerateSectorGrid(float size, float cellSize, float density, int maxIndex)
+    {
+        int gridDimensions = (int)(size / cellSize);
+        Debug.Log("Generating sector with grid dimensions " + gridDimensions + "x" + gridDimensions);
+        int[,] grid = new int[gridDimensions, gridDimensions];
+        for(int i = 0; i < gridDimensions; i++)
+        {
+            for(int j = 0;  j < gridDimensions; j++)
+            {
+                if(Random.value * 100f <= density)
+                {
+                    grid[i, j] = Random.Range(0, maxIndex);
+                }
+                else
+                {
+                    grid[i, j] = -1;
+                }
+            }
+        }
+        return grid;
+    }
+}
