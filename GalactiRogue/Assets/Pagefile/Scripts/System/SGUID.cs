2e31e6899f543b7ab7c5a06237499d55d87c0df0
pagefile <ahgaudette@gmail.com>
2021-07-05 18:35:25 -0500

    Adds InventoryItemData and first asset
    Adds InventoryItemData and FuelItemData asset. Also implements a serializable GUID class

---
+++ GalactiRogue/Assets/Pagefile/Scripts/System/SGUID.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class SGUID : ISerializationCallbackReceiver
+{
+    [SerializeField]
+    private string _strGuid;
+    private Guid _guid;
+
+    public static implicit operator SGUID(Guid guid) => new SGUID(guid);
+
+    public SGUID(Guid guid)
+    {
+        _guid = guid;
+        _strGuid = _guid.ToString();
+    }
+
+    public void OnAfterDeserialize()
+    {
+        _guid = Guid.Parse(_strGuid);
+    }
+
+    public void OnBeforeSerialize()
+    {
+        _strGuid = _guid.ToString();
+    }
+
+    static public bool operator==(SGUID l, SGUID r)
+    {
+        return l?._guid == r?._guid;
+    }
+
+    static public bool operator!=(SGUID l, SGUID r)
+    {
+        return l?._guid != r?._guid;
+    }
+
+    public override bool Equals(object obj)
+    {
+        SGUID guidobj = obj as SGUID;
+        return guidobj != null && _guid == guidobj._guid;
+    }
+
+    public override int GetHashCode()
+    {
+        return _guid.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return _guid.ToString();
+    }
+}
---
+++ GalactiRogue/Assets/Scripts/Data/InventoryItemData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/InventoryItemData", fileName = "NewInventoryItemData")]
+public class InventoryItemData : ScriptableObject
+{
+    [SerializeField]
+    private string _name = "NewItem";
+    [SerializeField]
+    private int _maxStackSize = 999;
+    [SerializeField]
+    private SGUID _itemID = default;
+
+    public string Name => _name;
+    public int MaxStackSize => _maxStackSize;
+
+    public static bool IsSameItem(InventoryItemData first, InventoryItemData second)
+    {
+        return first._itemID == second._itemID;
+    }
+
+    // This is so stupid. There needs to be a callback for creation in editor...
+    private void OnEnable()
+    {
+#if UNITY_EDITOR
+        // This shouldn't happen in a release build of the game
+        if(_itemID == default)
+        {
+            _itemID = Guid.NewGuid();
+            Debug.Log($"InventoryItemData.OnEnable: Creating GUID for item {_name} GUID: {_itemID}");
+        }
+#endif
+    }
+}
